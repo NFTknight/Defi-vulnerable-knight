8a96da229d6034f7c2ba975a45b9a4e64c437294
John Howard <117064821+NFTknight@users.noreply.github.com>
2022-10-31 00:03:17 -0700

    123
    hello
    
    Co-Authored-By: NFT Knight <108866734+JohnHoward940107@users.noreply.github.com>
    Co-Authored-By: Marko Frei <108885340+cryptodev1990@users.noreply.github.com>

--- contracts/truster/TrusterLenderPool.sol
+++ contracts/truster/TrusterLenderPool.sol
@@ -4,10 +4,9 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 contract TrusterLenderPool is ReentrancyGuard {
-
     IERC20 public damnValuableToken;
 
-    constructor (address tokenAddress) public {
+    constructor(address tokenAddress) public {
         damnValuableToken = IERC20(tokenAddress);
     }
 
@@ -16,19 +15,18 @@ contract TrusterLenderPool is ReentrancyGuard {
         address borrower,
         address target,
         bytes calldata data
-    )
-        external
-        nonReentrant
-    {
+    ) external nonReentrant {
         uint256 balanceBefore = damnValuableToken.balanceOf(address(this));
         require(balanceBefore >= borrowAmount, "Not enough tokens in pool");
-        
+
         damnValuableToken.transfer(borrower, borrowAmount);
         (bool success, ) = target.call(data);
         require(success, "External call failed");
 
         uint256 balanceAfter = damnValuableToken.balanceOf(address(this));
-        require(balanceAfter >= balanceBefore, "Flash loan hasn't been paid back");
+        require(
+            balanceAfter >= balanceBefore,
+            "Flash loan hasn't been paid back"
+        );
     }
-
 }
